c7e9f7565ca51aea186b95810781e8bbd88c0558
Gourab Chatterjee <114818987+Gourab-Kr-Chatterjee@users.noreply.github.com>
2024-04-18 05:55:34 +0000

    DataTypes In Js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version.
+
+// alert( 3 + 3) // we are using nodejs, not browser. Alert can be seen through browser only.
+
+console.log(3 
+    +
+     3) // code readability should be high. (Not recommended to write code like this)
+
+
+console.log("Gourav")
+
+// Datatypes In Js :-
+
+let name = "Gourav" // string
+let age = 18 //number or integer
+let isLoggedIn = false // Boolean
+let state; // Undefined
+
+// number => (2 to power 53)
+// bigint  (Used for large numbers)
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+// Know the type of datatype :-
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
