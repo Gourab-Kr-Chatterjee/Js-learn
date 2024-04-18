bab01f7471fd2effcc4d8111d1efae98bc6d1bf1
Gourab Chatterjee <114818987+Gourab-Kr-Chatterjee@users.noreply.github.com>
2024-04-18 07:28:45 +0000

    Conversion Operation

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score = "gourav"
+
+// procedure to know the type of a variable :-
+console.log(typeof score);
+console.log(typeof(score));
+
+
+// type conversion :-
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not a number) --> Type is Number.
+//  true => 1; false => 0
+
+let isLoggedIn = "gourav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "gourav" => true
